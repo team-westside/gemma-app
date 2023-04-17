0ba461fb52ff18a2b6a36b27772c71d7c9eb0448
Rishit Shivesh <rishitshivesh@gmail.com>
2023-04-17 05:53:34 +0530

    contracts added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,12 +4,48 @@ import Image from "next/image";
 import { FaUser } from "react-icons/fa";
 import { BsCart2 } from "react-icons/bs";
 import { AiFillHeart } from "react-icons/ai";
+import WalletCard from "../WalletCard/WalletCard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// import * as React from "react";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+// import PersonAdd from "@mui/icons-material/PersonAdd";
+// import Settings from "@mui/icons-material/Settings";
+// import Logout from "@mui/icons-material/Logout";
 
 const Navbar = () => {
   const items = ["Products", "Accesories", "About"];
-
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div className="w-[100vw] bg-white text-black flex flex-row justify-between py-4 px-10 items-center">
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <div className="max-w-[14vw]">
         <img src={logoname.src} alt="logo" className="object-contain" />
       </div>
@@ -22,7 +58,90 @@ const Navbar = () => {
           );
         })}
       </div>
-      <div className="flex flex-row justify-evenly gap-x-3">
+      <div className="flex flex-row justify-evenly gap-x-3 items-center">
+        {/* <WalletCard /> */}
+        <React.Fragment>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              textAlign: "center",
+              padding: 1,
+            }}
+          >
+            <Tooltip title="Account settings">
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? "account-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+              </IconButton>
+            </Tooltip>
+          </Box>
+          <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                mt: 1.5,
+                padding: "10px 10px",
+                "& .MuiAvatar-root": {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
+                },
+                "&:before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <WalletCard />
+            <MenuItem>
+              <Avatar /> Profile
+            </MenuItem>
+            <MenuItem>
+              <Avatar /> My account
+            </MenuItem>
+            <Divider />
+            <MenuItem>
+              <ListItemIcon>
+                {/* <PersonAdd fontSize="small" /> */}
+              </ListItemIcon>
+              Add another account
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>{/* <Settings fontSize="small" /> */}</ListItemIcon>
+              Settings
+            </MenuItem>
+            <MenuItem>
+              <ListItemIcon>{/* <Logout fontSize="small" /> */}</ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
+        </React.Fragment>
         <FaUser className="text-2xl hover:text-[#F9A826]  cursor-pointer transition-all" />
         <AiFillHeart className="text-2xl hover:text-[#ba9d6d] cursor-pointer transition-all" />
         <BsCart2 className="text-2xl hover:text-[#F9A826] cursor-pointer transition-all" />
---
+++ src/components/WalletCard/WalletCard.js
@@ -0,0 +1,165 @@
+import React, { useEffect, useState } from "react";
+// import { Button } from '@material-ui/core';
+import Ethereum from "../../assets/Ethereum.png";
+import { ethers } from "ethers";
+import axios from "axios";
+import { stringify } from "flatted";
+import abi from "../../data/jaAssusre.json";
+import { toast } from "react-toastify";
+// const provider = new ethers.providers.Web3Provider(window.Ethereum);
+// const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const WalletCard = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [accounts, setAccounts] = useState(0);
+  const [transaction, setTransaction] = useState(null);
+  const getUser = async (provider) => {
+    const account = await provider.send("eth_requestAccounts").then((data) => {
+      //   console.log(data);
+      setAccounts(data);
+      setDefaultAccount(data[0]);
+    });
+    return account;
+  };
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      const ethers = require("ethers");
+      //   console.log(ethers, ethers.provider, window.ethereum);
+      if (window.ethereum && window.ethereum.isMetaMask) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        setProvider(provider);
+        // request user's permission to connect their MetaMask account
+        getUser(provider);
+
+        // window.alert(`Connected account: ${accounts}`);
+      } else {
+        window.alert("Please install MetaMask");
+      }
+    }
+  }, []);
+  //   console.log(provider);
+  //   let provider = new ethers.providers.Web3Provider(web3.currentProvider);
+  const connectwalletHandler = () => {
+    if (window.Ethereum) {
+      console.log(provider);
+      provider.send("eth_requestAccounts", []).then(async () => {
+        console.log(provider);
+        await accountChangedHandler(provider.getSigner());
+      });
+    } else {
+      setErrorMessage("Please Install Metamask!!!");
+    }
+  };
+  const accountChangedHandler = async (newAccount) => {
+    const address = await newAccount.getAddress();
+    setDefaultAccount(address);
+    const balance = await newAccount.getBalance();
+    console.log(balance);
+    setUserBalance(ethers.utils.formatEther(balance));
+    await getuserBalance(address);
+  };
+  const getuserBalance = async (address) => {
+    const balance = await provider
+      .getBalance(address, "latest")
+      .then((data) => {
+        console.log(data);
+        setUserBalance(ethers.utils.formatEther(data));
+        return data;
+      });
+    // console.log(balance);
+    // return balance;
+  };
+  //   console.log(CONTRACT_ADDRESS);
+  useEffect(() => {
+    if (defaultAccount) {
+      const balance = getuserBalance(defaultAccount);
+      console.log(balance);
+
+      //   setUserBalance(ethers.utils.formatEther(balance));
+    }
+  }, [defaultAccount]);
+  //   useEffect(() => {
+  //     accountChangedHandler(defaultAccount);
+  //   }, [defaultAccount]);
+  const handleData = async (data) => {
+    toast.info("Transaction in progress");
+    const res = await data.wait();
+    setTransaction(res);
+    toast.success("Transaction Successfull");
+    console.log(res);
+    return res;
+  };
+  //   toast.success("Transaction Successfull");
+  const handleClick = async () => {
+    const contractAddress = "0x4Dbd29725Ad734280Db4F57F5CB387E2a042A6E3";
+    if (!contractAddress) {
+      window.alert("Please set the contract address in the .env file");
+    }
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+
+    // const res = await contract.checkUserRegistration();
+    // console.log(res);
+    console.log("contract created");
+    await provider.send("eth_requestAccounts", []); // <- this promps user to connect metamask
+    // console.log(signer);
+    // const check = await contract.checkUserRegistration();
+    const check = false;
+    console.log(check);
+    if (!check) {
+      const result = await contract.registerUser().then((data) => {
+        handleData(data);
+        console.log(data);
+        //   console.log(data.wait());
+      });
+      console.log("Data added to Blockchain!!");
+      console.log(result);
+    } else {
+      toast.info("User Already Registered");
+    }
+  };
+  return (
+    <div className="WalletCard">
+      {/* <h3 className="h4">Welcome to a decentralized Application</h3> */}
+      <div
+        className="w-[10px] h-[10px] rounded-full absolute right-2 top-2"
+        style={{
+          background: defaultAccount ? "#A5CC82" : "red",
+        }}
+      ></div>
+      {!defaultAccount && (
+        <button
+          onClick={connectwalletHandler}
+          className="bg-[#ecab4f] text-white cursor-pointer mx-auto px-5 py-2 hover:opacity-75 transition-all"
+        >
+          Connect Wallet
+        </button>
+      )}
+      {/* <button
+        style={{ background: defaultAccount ? "#A5CC82" : "white" }}
+        onClick={connectwalletHandler}
+      >
+        {defaultAccount ? "Connected!!" : "Connect"}
+      </button> */}
+      <div className="displayAccount">
+        <h4 className="walletAddress">
+          Address:{String(defaultAccount).substring(0, 10)}*******
+        </h4>
+        <div className="flex flex-row items-center justify-center gap-x-4">
+          <img src={Ethereum.src} className="App-logo w-[20px]" alt="logo" />
+          <div className="flex flex-col justify-center">
+            <h3 className="font-bold text-lg">Wallet</h3>
+            <h3>ETH {userBalance}</h3>
+          </div>
+          {/* <br /> */}
+        </div>
+      </div>
+      {errorMessage}
+      <div onClick={handleClick}>Click Me</div>
+      {/* {provider && stringify(provider)} */}
+    </div>
+  );
+};
+export default WalletCard;
