af34dc47cbc203ed20b33747d087ee13ffd3ab45
Rishit Shivesh <rishitshivesh@gmail.com>
2023-04-18 16:04:25 +0530

    add backend

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/api/v1";
+const API_URL = "https://gemma-backend.vercel.app/api/v1";
 // const API_URL = process.env.API_URL;
 //
 // const TOKEN = process.env.TOKEN;
